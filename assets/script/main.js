bd44c0a61f76bf34e5ca612c77812548a7ab87c6
pandanokokoro <pandanokokoro41@gmail.com>
2024-11-30 20:03:07 +0900

    commit 20241130

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,28 +1,28 @@
 //ハンバーガー
 $(document).ready(function() {
     $('#hamburger-button').on('click', function() {
-    $('.navigation-group').toggleClass('active');
-});
+        $('.navigation-group').toggleClass('active');
+    });
 
-  // ナビゲーションアイテムをクリックしたときにメニューを閉じる
+    // ナビゲーションアイテムをクリックしたときにメニューを閉じる
     $('.navigation-item a').on('click', function() {
-    $('.navigation-group').removeClass('active');
-});
-    $('.header-button a').on('click', function() {
-    $('.navigation-group').removeClass('active');
-});
-
-  // 画面幅を変更したときにメニューを閉じる
-$(window).on('resize', function() {
-    if ($(window).width() > 768) {
         $('.navigation-group').removeClass('active');
+    });
+    $('.header-button a').on('click', function() {
+        $('.navigation-group').removeClass('active');
+    });
+
+    // 画面幅を変更したときにメニューを閉じる
+    $(window).on('resize', function() {
+        if ($(window).width() > 768) {
+            $('.navigation-group').removeClass('active');
         }
     });
-});
+    });
 
 // タブ
 $('#smarttab').smartTab({
-    enableUrlHash: false,
+	enableUrlHash: false,
 });
 
 
